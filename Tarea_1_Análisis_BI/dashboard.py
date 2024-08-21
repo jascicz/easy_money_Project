1177307473076fd6c1d1e5a3556cc403e6a52e02
jas <154901052+jascicz@users.noreply.github.com>
2024-08-21 09:11:25 +0200

    Update dashboard.py

--- Tarea_1_Análisis_BI/dashboard.py
+++ Tarea_1_Análisis_BI/dashboard.py
@@ -22,7 +22,7 @@ if st.session_state.page == "welcome":
     # Botón para acceder al dashboard
             if st.button("Acceder al Dashboard"):
                 st.session_state.page = "dashboard"
-                st.experimental_set_query_params(page="dashboard")
+                st.stop()
 
 # Aquí sigue el código del dashboard o el contenido principal
 elif st.session_state.page == "dashboard":
